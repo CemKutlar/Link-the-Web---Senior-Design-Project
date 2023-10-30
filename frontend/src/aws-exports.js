6df482ad2bacfe3cfbb9aa50fd3173faef2b3c73
Oktay Cem Kutlar <101462838+CemKutlar@users.noreply.github.com>
2023-10-30 14:46:36 +0300

    Fix error

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -1,5 +1,7 @@
-export const awsExports = {
+const awsExports = {
   REGION: "us-east-1",
   USER_POOL_ID: "us-east-1_CX8YUvj2P",
   USER_POOL_CLIENT_ID: "1hk52tg0mqsvvsjkoulfgasesk",
 };
+
+export default awsExports;
