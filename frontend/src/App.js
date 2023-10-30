65aecdd0f3cac3c96ac21c99a3d85e982ffccbb3
Oktay Cem Kutlar <101462838+CemKutlar@users.noreply.github.com>
2023-10-30 12:40:15 +0300

    Add login feature. Set AWS cognito. Set amplify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,94 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Amplify } from "aws-amplify";
+import { awsExports } from "./aws-exports";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+Amplify.configure({
+  Auth: {
+    region: awsExports.REGION,
+    userPoolId: awsExports.USER_POOL_ID,
+    userPoolWebClientId: awsExports.USER_POOL_CLIENT_ID,
+  },
+});
+
+const formFields = {
+  signIn: {
+    username: {
+      placeholder: "Enter your email:",
+    },
+  },
+  signUp: {
+    username: {
+      placeholder: "Enter your username:",
+      order: 1,
+    },
+    confirm_password: {
+      label: "Confirm Password:",
+      placeholder: "Confirm your password:",
+      order: 2,
+    },
+    password: {
+      label: "Password:",
+      placeholder: "Enter your password:",
+      isRequired: false,
+      order: 3,
+    },
+    name: {
+      label: "Full Name:",
+      placeholder: "Enter your full name:",
+      order: 4,
+    },
+    nickname: {
+      label: "Nickname:",
+      placeholder: "Enter your nickname:",
+      order: 5,
+    },
+    email: {
+      label: "Email:",
+      placeholder: "Enter your email:",
+      order: 6,
+    },
+    gender: {
+      label: "Gender:",
+      placeholder: "Select your gender:",
+      order: 7,
+    },
+    birthdate: {
+      label: "Birthdate:",
+      placeholder: "Enter your birthdate:",
+      order: 8,
+    },
+    phone_number: {
+      label: "Phone Number:",
+      placeholder: "Enter your phone number:",
+      order: 9,
+    },
+  },
+  setupTOTP: {
+    QR: {
+      totpIssuer: "test issuer",
+      totpUsername: "amplify_qr_test_user",
+    },
+    confirmation_code: {
+      label: "New Label",
+      placeholder: "Enter your confirmation code:",
+      isRequired: false,
+    },
+  },
+  confirmSignIn: {
+    confirmation_code: {
+      label: "New Label",
+      placeholder: "Enter your confirmation code:",
+      isRequired: false,
+    },
+  },
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator formFields={formFields}>
+      {({ signOut }) => <button onClick={signOut}>Sign out</button>}
+    </Authenticator>
   );
 }
-
-export default App;
---
+++ frontend/src/aws-exports.js
@@ -0,0 +1,5 @@
+export const awsExports = {
+  REGION: "us-east-1",
+  USER_POOL_ID: "us-east-1_CX8YUvj2P",
+  USER_POOL_CLIENT_ID: "1hk52tg0mqsvvsjkoulfgasesk",
+};
