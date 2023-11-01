e2379a8aafa1cca67faf779695e1de1073fa55fc
Oktay Cem Kutlar <101462838+CemKutlar@users.noreply.github.com>
2023-11-01 15:54:09 +0300

    Add to link page

--- frontend/src/LinkPage.js
+++ frontend/src/LinkPage.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { SearchField } from "@aws-amplify/ui-react";
 import styles from "./styles/LinkPage.module.css"; // Import the CSS module
+import { useState } from "react";
+import "@aws-amplify/ui-react/styles.css";
 
 const LinkPage = () => {
   return (
@@ -11,8 +13,70 @@ const LinkPage = () => {
         placeholder="Search here..."
         labelHidden={false}
       />
+      <div className="comment-section">
+        <h2>Comments</h2>
+        <div className="new-comment">
+          <textarea placeholder="Add a comment..."></textarea>
+          <button>Post Comment</button>
+        </div>
+        {dummyComments.map((comment) => (
+          <Comment key={comment.id} data={comment} />
+        ))}
+      </div>
     </div>
   );
 };
 
+const dummyComments = [
+  {
+    id: 1,
+    text: "This is a comment",
+    votes: 10,
+    replies: [
+      { id: 1, text: "This is a reply to the comment", votes: 5 },
+      { id: 2, text: "Another reply to the comment", votes: 2 },
+    ],
+  },
+  {
+    id: 2,
+    text: "Another top-level comment",
+    votes: 8,
+    replies: [],
+  },
+];
+
+const Comment = ({ data }) => {
+  const [votes, setVotes] = useState(data.votes);
+
+  return (
+    <div className="comment">
+      <p>{data.text}</p>
+      <div className="votes">
+        <button onClick={() => setVotes(votes + 1)}>Upvote</button>
+        <span>{votes}</span>
+        <button onClick={() => setVotes(votes - 1)}>Downvote</button>
+      </div>
+      <div className="replies">
+        {data.replies.map((reply) => (
+          <Reply key={reply.id} data={reply} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Reply = ({ data }) => {
+  const [votes, setVotes] = useState(data.votes);
+
+  return (
+    <div className="reply">
+      <p>{data.text}</p>
+      <div className="votes">
+        <button onClick={() => setVotes(votes + 1)}>Upvote</button>
+        <span>{votes}</span>
+        <button onClick={() => setVotes(votes - 1)}>Downvote</button>
+      </div>
+    </div>
+  );
+};
 export default LinkPage;
