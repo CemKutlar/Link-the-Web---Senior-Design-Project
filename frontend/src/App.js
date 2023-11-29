6804166a0b002ca4453d9be72f31988e2f091ccf
Oktay Cem Kutlar <101462838+CemKutlar@users.noreply.github.com>
2023-11-29 19:55:35 +0300

    Try to fix SASS error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -124,7 +124,7 @@ export default function App() {
     // <div className="gradient-bg">
     //   <svg xmlns="http://www.w3.org/2000/svg">
     //     <defs>
-    //       <filter id="dsagdssdsohgkömsdaadsffsfdsfsfsdafdsjjk">
+    //       <filter id="dsagdssdsohgkömsdaadsdsffsfdsfsfsdafdsjjk">
     //         <feGaussianBlur
     //           in="SourceGraphic"
     //           stdDeviation="10"
