9307181ea0c812b30bd7dd801525720fe2c24000
Oktay Cem Kutlar <101462838+CemKutlar@users.noreply.github.com>
2023-12-06 14:55:42 +0300

    Fix SQL query problem occurs when searching a link name

--- frontend/src/components/Link.js
+++ frontend/src/components/Link.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 
 export function Linkk() {
   const { id } = useParams();
+  console.log("Linkk'e girdi");
   console.log("Link ID:", id);
   return <div>Hi, the link ID is {id}</div>;
 }
--- frontend/src/contexts/LinkContext.js
+++ frontend/src/contexts/LinkContext.js
@@ -7,7 +7,19 @@ const Context = React.createContext();
 
 export function LinkProvider({ children }) {
   const { id } = useParams();
+  console.log(id);
   const { loading, error, value: link } = useAsync(() => getLink(id), [id]);
-  console.log(link);
-  return <Context.Provider value={{}}>{children}</Context.Provider>;
+
+  if (loading) {
+    return <div>Loading...</div>; // Or any loading indicator
+  }
+
+  if (error) {
+    console.error("Error in LinkProvider:", error);
+    return <div>Error: {error.message}</div>; // Display error message
+  }
+
+  console.log(link); // Now, link should not be undefined if there's no error and loading is done
+
+  return <Context.Provider value={{ link }}>{children}</Context.Provider>;
 }
